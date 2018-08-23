347cdefbb710b8e91ff47b6640572144c783a50e
Ilya Kotelnikov <ilya.kotelnikov@gmail.com>
2018-08-23 09:46:23 +0700

    In makefile add a support for MINICRT user-define to build the dll with no system CRT library. Export from the dll an empty DllMain() as the direct entry point in case of MINICRT build.

---
+++ dllmain.cpp
@@ -0,0 +1,19 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2018, Ilya Kotelnikov (ilya.kotelnikov@gmail.com)
+// All rights reserved.
+//
+// You may use, distribute and modify this code under the terms of GNU GPLv3.
+//------------------------------------------------------------------------------
+
+#include <windows.h>
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE module_handle, DWORD reason, LPVOID) {
+  switch (reason) {
+    case DLL_PROCESS_ATTACH:
+      break;
+
+    case DLL_PROCESS_DETACH:
+      break;
+  }
+  return TRUE;
+}
